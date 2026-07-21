788d5c78bd9adf9259312cdc5596e71c217307d3
masa19810503-wq <masa19810503@gmail.com>
2026-07-21 15:54:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const ASSETS = [
   '/index.html',
   '/manifest.json',
   '/icon-192.png',
-  '/icon-512.png'
+  '/icon-512.png',
+  '/nylon-sling-chart.png'
 ];
 
 self.addEventListener('install', e => {
